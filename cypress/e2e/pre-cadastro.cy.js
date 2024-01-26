63ba5f377a6d950390d0b50fb45dc8a9b55d94bb
SchunckRenan <renanschunck@gmail.com>
2024-01-26 15:30:09 +0000

    Corrigir pequenos problemas em pre-cadastro

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -9,13 +9,17 @@ describe('Funcionalidade Pré Cadastro', () => {
     });
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+        let nomeFaker = faker.person.firstName()
+        let sobrenomeFaker = faker.person.lastName()
+        let emailFaker = faker.internet.email()
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type(faker.internet.password())
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
         
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -5,16 +5,16 @@ describe('Funcionalidade Página de produtos', () => {
     beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
     });
-    
+
     it('Deve selecionar um da lista', () => {
         cy.get('[class="product-block grid"]')
-        ///.first() Seleciona o primeiro item da lista
-        ///.last() Seleciona o último item da lista
-        ///.eq(3) É usado para escolher a posição específica de uma lista de 0 a 99999(0=1, 1=2, 2=3...)
-        ///.contains('Ariel Roll Sleeve Sweatshirt') Busca um item pelo nome contido no elemento.
-        .first()
-        .click() 
-        
+            ///.first() Seleciona o primeiro item da lista
+            ///.last() Seleciona o último item da lista
+            ///.eq(3) É usado para escolher a posição específica de uma lista de 0 a 99999(0=1, 1=2, 2=3...)
+            ///.contains('Ariel Roll Sleeve Sweatshirt') Busca um item pelo nome contido no elemento.
+            .first()
+            .click()
+
     });
 
     it.only('Deve adicionar um produto ao carrinho', () => {
@@ -22,25 +22,25 @@ describe('Funcionalidade Página de produtos', () => {
         var quant = 5
 
         cy.get('[class="product-block grid"]')
-        .first()
-        .click()
+            .first()
+            .click()
         cy.get('.button-variable-item-XL')
-        .click()
+            .click()
         cy.get('.button-variable-item-Red')
-        .click()
+            .click()
         cy.get('.button-variable-item-XL') ///Enjambre detected!
-        .click()
+            .click()
         cy.get('.input-text')
-        .clear()
-        .type(quant)
+            .clear()
+            .type(quant)
         cy.get('.single_add_to_cart_button')
-        .click()
+            .click()
 
         cy.get('.dropdown-toggle > .mini-cart-items')
-        .should('contain', quant)
+            .should('contain', quant)
         cy.get('.woocommerce-message')
-        .should('contain', quant + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
-        
+            .should('contain', quant + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+
     });
 
 })
