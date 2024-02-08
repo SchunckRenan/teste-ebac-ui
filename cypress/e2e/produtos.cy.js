31a6102a0039f40c2cb1b9129c9c24ab7f5a066c
SchunckRenan <renanschunck@gmail.com>
2024-02-08 15:35:33 +0000

    Adição de testes em produtos

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -7,7 +7,7 @@ describe('Funcionalidade Página de produtos', () => {
         produtosPage.visitaUrl()
     });
 
-    it('Deve selecionar um da lista', () => {
+    it('Deve selecionar um produto da lista', () => {
         produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
 
@@ -37,17 +37,37 @@ describe('Funcionalidade Página de produtos', () => {
 
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Aether Gym Pant'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
-    it('Deve buscar um produto com sucesso', () => {
-        
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto("apollo running short/")
+        cy.get('.product_title').should('contain', 'Apollo Running Short')
     });
 
     it('Deve adicionar um produto ao carrinho', () => {
+        let quant = 7
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCart('L', 'Blue', quant)
+
+        cy.get('.woocommerce-message').should('contain', quant + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar um produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[1].nomeProduto)
+        produtosPage.addProdutoCart(
+            dados[1].tamanho,
+            dados[1].cor,
+            dados[1].qtd)
+
+        cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+
+        })
+        
         
     });
 
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -15,9 +15,23 @@ class ProdutosPage {
             .click()
     }
 
-    addProdutoCart() {
-        //
+    addProdutoCart(tamanho, cor, qtd) {
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.button-variable-item-' + tamanho).click() //enjambre
+        cy.get('.input-text').clear().type(qtd)
+        cy.get('.single_add_to_cart_button').click()
+
     }
+
+    visitarProduto(nomeProduto) {
+        //cy.visit(`produtos/${nomeProduto}`)
+        //Interpolação é uma forma de concatenar usando crase/${varável}crase
+
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
+    }   
+
 }
 
 export default new ProdutosPage
