c30171a5ec20d3bbc8157f24b52d5254fd58dd10
SchunckRenan <renanschunck@gmail.com>
2024-02-09 11:18:17 +0000

    mudança no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://lojaebac.ebaconline.art.br"
+    baseUrl: "http://lojaebac.ebaconline.art.br",
+    video: true,
+    projectId: "b6pneh"
   },
 });
 
